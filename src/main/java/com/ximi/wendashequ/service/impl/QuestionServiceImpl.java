e3b4558e14effec25377a61de6727375ae5d335f
aikuyun <1534761445@qq.com>
2017-10-18 09:09:08 +0800

    简单的过滤掉html脚本 By  tsl

--- src/main/java/com/ximi/wendashequ/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/ximi/wendashequ/service/impl/QuestionServiceImpl.java
@@ -5,6 +5,7 @@ import com.ximi.wendashequ.model.Question;
 import com.ximi.wendashequ.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -24,6 +25,9 @@ public class QuestionServiceImpl implements QuestionService {
 
     @Override
     public int addQuestion(Question question) {
+        //使用HtmlUtils工具来对输入的内容进行转义
+        question.setTitle(HtmlUtils.htmlEscape(question.getTitle()));
+        question.setContent(HtmlUtils.htmlEscape(question.getContent()));
         return questionDAO.addQuestion(question);
     }
 }
